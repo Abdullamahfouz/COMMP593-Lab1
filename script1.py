0f2becd08c71a7b1f738a750b333ec6975a3e5fe
Abdullamahfouz <abdullah.mahfouz@flemingcollege.ca>
2023-01-18 13:34:07 -0500

    Added comment to script1.py

--- script1.py
+++ script1.py
@@ -1,2 +1,3 @@
+# Print Lines from Zen of python 
 print("There should be one-- and preferably only pn -- obvious way to do it")
 print("Although that way may not be obvious at first unless you're Dutch")
