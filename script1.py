8e6a7a09e9e8b452478d801d1e9ad905dc7dae64
Abdullamahfouz <abdullah.mahfouz@flemingcollege.ca>
2023-01-18 14:15:15 -0500

    Completed new feature
    Added good feature to script1.py

--- script1.py
+++ script1.py
@@ -1,3 +1,4 @@
 # Print Lines from Zen of python 
 print("There should be one-- and preferably only pn -- obvious way to do it")
-print("Although that way may not be obvious at first unless you're Dutch")
+
+# this is an amazing feature
--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 # Print Lines from Zen of python
 print("My fav team is real Madrid ")
 print("I hate Dark chocolate ew")
+print("I hate Dark chocolate ew")
